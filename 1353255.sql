c1219246e7395d444ad8b4c92fad600423882345
RamaKaorma <rkaorma@student.unimelb.edu.au>
2023-04-27 19:08:26 +1000

    Task 7 done... (maybe)

--- 1353255.sql
+++ 1353255.sql
@@ -68,7 +68,7 @@ SELECT MAX(count) FROM (
 	SELECT referencedPublicationId, COUNT(referencedPublicationId) AS count 
 	FROM referencing 
 	GROUP BY referencedPublicationId 
-) as maxCount );
+) AS maxCount );
 
 -- END Q4
 -- ____________________________________________________________________________________________________________________________________________________________________________________________________________
@@ -123,9 +123,21 @@ WHERE researcher.Id IN (
 -- ____________________________________________________________________________________________________________________________________________________________________________________________________________
 -- BEGIN Q7
 
-
-
-
+SELECT DISTINCT keyword.Id, keyword.Word
+FROM keyword
+JOIN publication_has_keyword ON keyword.Id = publication_has_keyword.keywordID
+WHERE publication_has_keyword.keywordID IN (
+	SELECT DISTINCT PublicationId
+	FROM topten
+	GROUP BY PublicationId
+	HAVING COUNT(PublicationId) = (
+		SELECT MAX(count) FROM (
+			SELECT PublicationId, COUNT(PublicationId) AS count 
+			FROM topten 
+			GROUP BY PublicationId 
+		) AS maxCount
+	)
+);
 
 -- END Q7
 -- ____________________________________________________________________________________________________________________________________________________________________________________________________________
